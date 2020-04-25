a57ba7f585b727cfcdcf03320a1bfa1aa61f52b6
nirae <hello@nicolasdubouilh.fr>
2020-04-25 21:09:04 +0200

    continue level05

--- level05/source.c
+++ level05/source.c
@@ -12,7 +12,7 @@ int main(void) {
     0x08048452 <+14>:	mov    DWORD PTR [esp+0x8c],0x0
         esp+0x8c = 140 ; 144 -140 = 4 ; int?
     */
-    int var1 = 0; /* esp+0x8c */
+    int i = 0; /* esp+0x8c */
     char buffer[100]; /* esp+0x28 */
     /*
     0x0804845d <+25>:	mov    eax,ds:0x80497f0
@@ -27,25 +27,8 @@ int main(void) {
     /*
     0x0804847a <+54>:	mov    DWORD PTR [esp+0x8c],0x0
     0x08048485 <+65>:	jmp    0x80484d3 <main+143>
-    0x08048487 <+67>:	lea    eax,[esp+0x28]
-    0x0804848b <+71>:	add    eax,DWORD PTR [esp+0x8c]
-    0x08048492 <+78>:	movzx  eax,BYTE PTR [eax]
-    0x08048495 <+81>:	cmp    al,0x40
-    0x08048497 <+83>:	jle    0x80484cb <main+135>
-    0x08048499 <+85>:	lea    eax,[esp+0x28]
-    0x0804849d <+89>:	add    eax,DWORD PTR [esp+0x8c]
-    0x080484a4 <+96>:	movzx  eax,BYTE PTR [eax]
-    0x080484a7 <+99>:	cmp    al,0x5a
-    0x080484a9 <+101>:	jg     0x80484cb <main+135>
-    0x080484ab <+103>:	lea    eax,[esp+0x28]
-    0x080484af <+107>:	add    eax,DWORD PTR [esp+0x8c]
-    0x080484b6 <+114>:	movzx  eax,BYTE PTR [eax]
-    0x080484b9 <+117>:	mov    edx,eax
-    0x080484bb <+119>:	xor    edx,0x20
-    0x080484be <+122>:	lea    eax,[esp+0x28]
-    0x080484c2 <+126>:	add    eax,DWORD PTR [esp+0x8c]
-    0x080484c9 <+133>:	mov    BYTE PTR [eax],dl
-    0x080484cb <+135>:	add    DWORD PTR [esp+0x8c],0x1
+    */
+    /*
     0x080484d3 <+143>:	mov    ebx,DWORD PTR [esp+0x8c]
     0x080484da <+150>:	lea    eax,[esp+0x28]
     0x080484de <+154>:	mov    DWORD PTR [esp+0x1c],0xffffffff
@@ -53,16 +36,58 @@ int main(void) {
     0x080484e8 <+164>:	mov    eax,0x0
     0x080484ed <+169>:	mov    ecx,DWORD PTR [esp+0x1c]
     0x080484f1 <+173>:	mov    edi,edx
-    0x080484f3 <+175>:	repnz scas al,BYTE PTR es:[edi]
+    0x080484f3 <+175>:	repnz scas al,BYTE PTR es:[edi]  strlen on buffer
     0x080484f5 <+177>:	mov    eax,ecx
     0x080484f7 <+179>:	not    eax
     0x080484f9 <+181>:	sub    eax,0x1
     0x080484fc <+184>:	cmp    ebx,eax
     0x080484fe <+186>:	jb     0x8048487 <main+67>
+    */
+    while (i < strlen(buffer)) {
+        /*
+        0x08048487 <+67>:	lea    eax,[esp+0x28]
+        0x0804848b <+71>:	add    eax,DWORD PTR [esp+0x8c]     buffer[i]
+        0x08048492 <+78>:	movzx  eax,BYTE PTR [eax]
+        0x08048495 <+81>:	cmp    al,0x40
+        0x08048497 <+83>:	jle    0x80484cb <main+135>
+        */
+        if (buffer[i] > 64) {
+            /*
+            0x08048499 <+85>:	lea    eax,[esp+0x28]
+            0x0804849d <+89>:	add    eax,DWORD PTR [esp+0x8c]     buffer[i]
+            0x080484a4 <+96>:	movzx  eax,BYTE PTR [eax]
+            0x080484a7 <+99>:	cmp    al,0x5a
+            0x080484a9 <+101>:	jg     0x80484cb <main+135>
+            */
+            if (buffer[i] < 90) {
+                /*
+                0x080484ab <+103>:	lea    eax,[esp+0x28]
+                0x080484af <+107>:	add    eax,DWORD PTR [esp+0x8c]     buffer[i]
+                0x080484b6 <+114>:	movzx  eax,BYTE PTR [eax]
+                0x080484b9 <+117>:	mov    edx,eax
+                0x080484bb <+119>:	xor    edx,0x20                     buffer[i] ^ 32
+                0x080484be <+122>:	lea    eax,[esp+0x28]
+                0x080484c2 <+126>:	add    eax,DWORD PTR [esp+0x8c]     buffer[i]
+                0x080484c9 <+133>:	mov    BYTE PTR [eax],dl            
+                */
+                buffer[i] = buffer[i] ^ 32
+            }
+        }
+        /*
+        0x080484cb <+135>:	add    DWORD PTR [esp+0x8c],0x1
+        */
+        i++;
+    }
+    /*
     0x08048500 <+188>:	lea    eax,[esp+0x28]
     0x08048504 <+192>:	mov    DWORD PTR [esp],eax
     0x08048507 <+195>:	call   0x8048340 <printf@plt>
+    */
+    printf(buffer); /* FORMAT STRING */
+    /*
     0x0804850c <+200>:	mov    DWORD PTR [esp],0x0
     0x08048513 <+207>:	call   0x8048370 <exit@plt>\
     */
+    exit(0);
+    return 0;
 }
--- level05/walkthrough.md
+++ level05/walkthrough.md
@@ -10,3 +10,19 @@ Write the input
 
 Start in GDB
 
+There is a `fgets()` call, stored the input on a buffer of 100 chars. A loop on the buffer for transform the input into lowercase.
+
+At the end, there is a call to `printf()` with the buffer as parameter, this is a format string
+
+    0x08048500 <+188>:	lea    eax,[esp+0x28]   ; buffer
+    0x08048504 <+192>:	mov    DWORD PTR [esp],eax
+    0x08048507 <+195>:	call   0x8048340 <printf@plt>
+
+Let's try it
+
+    $ python -c 'print "BBBB"+"-%x"*12' | ./level05 
+    bbbb-64-f7fcfac0-f7ec3af9-ffffd5ff-ffffd5fe-0-ffffffff-ffffd684-f7fdb000-62626262-2d78252d-252d7825
+
+The format string works and we can find our input at the 10th position
+
+To exploit it, we can store a shellcode in an env variable and inject it at the 10th position with the format string
