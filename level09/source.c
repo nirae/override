f50d67f17178714fb9bf34ffcf9de55894573181
nirae <hello@nicolasdubouilh.fr>
2020-05-07 17:38:37 +0200

    level09 source

--- level09/source.c
+++ level09/source.c
@@ -1,3 +1,9 @@
+struct s_message {
+    char message[140];  /* message */
+    char username[40];  /* message+140 */
+    int len_message;    /* message+180 */
+};
+
 void secret_backdoor() {
     /*
     0x000000000000088c <+0>:	push   rbp
@@ -7,36 +13,61 @@ void secret_backdoor() {
     0x000000000000089b <+15>:	mov    rax,QWORD PTR [rax]
     0x000000000000089e <+18>:	mov    rdx,rax
     0x00000000000008a1 <+21>:	lea    rax,[rbp-0x80]
+        rbp-0x80 = 128
+    */
+    char buffer[128]; /* rbp-0x80 */
+    /*
     0x00000000000008a5 <+25>:	mov    esi,0x80
     0x00000000000008aa <+30>:	mov    rdi,rax
     0x00000000000008ad <+33>:	call   0x770 <fgets@plt>
+    */
+    fgets(buffer, 128, stdin);
+    /*
     0x00000000000008b2 <+38>:	lea    rax,[rbp-0x80]
     0x00000000000008b6 <+42>:	mov    rdi,rax
     0x00000000000008b9 <+45>:	call   0x740 <system@plt>
+    */
+    system(buffer);
+    /*
     0x00000000000008be <+50>:	leave  
     0x00000000000008bf <+51>:	ret  
     */
+    return;
 }
 
-void set_username() {
+void set_username(struct s_message *message) {
     /*
     0x00000000000009cd <+0>:	push   rbp
     0x00000000000009ce <+1>:	mov    rbp,rsp
     0x00000000000009d1 <+4>:	sub    rsp,0xa0
-    0x00000000000009d8 <+11>:	mov    QWORD PTR [rbp-0x98],rdi
+        total stack = 160
+    0x00000000000009d8 <+11>:	mov    QWORD PTR [rbp-0x98],rdi     ; arg
     0x00000000000009df <+18>:	lea    rax,[rbp-0x90]
+        rbp-0x90 = 144
+    */
+    char buffer[128]; /* rbp-0x90 */
+    /*
     0x00000000000009e6 <+25>:	mov    rsi,rax
     0x00000000000009e9 <+28>:	mov    eax,0x0
-    0x00000000000009ee <+33>:	mov    edx,0x10
+    0x00000000000009ee <+33>:	mov    edx,0x10     16
     0x00000000000009f3 <+38>:	mov    rdi,rsi
     0x00000000000009f6 <+41>:	mov    rcx,rdx
-    0x00000000000009f9 <+44>:	rep stos QWORD PTR es:[rdi],rax
+    0x00000000000009f9 <+44>:	rep stos QWORD PTR es:[rdi],rax     ; bzero
+    */
+    bzero(buffer, 1128);
+    /*
     0x00000000000009fc <+47>:	lea    rdi,[rip+0x1e1]        # 0xbe4
     0x0000000000000a03 <+54>:	call   0x730 <puts@plt>
+    */
+    puts(">: Enter your username");
+    /*
     0x0000000000000a08 <+59>:	lea    rax,[rip+0x1d0]        # 0xbdf
     0x0000000000000a0f <+66>:	mov    rdi,rax
     0x0000000000000a12 <+69>:	mov    eax,0x0
     0x0000000000000a17 <+74>:	call   0x750 <printf@plt>
+    */
+    printf(">>: ");
+    /*
     0x0000000000000a1c <+79>:	mov    rax,QWORD PTR [rip+0x201595]        # 0x201fb8
     0x0000000000000a23 <+86>:	mov    rax,QWORD PTR [rax]
     0x0000000000000a26 <+89>:	mov    rdx,rax
@@ -44,16 +75,17 @@ void set_username() {
     0x0000000000000a30 <+99>:	mov    esi,0x80
     0x0000000000000a35 <+104>:	mov    rdi,rax
     0x0000000000000a38 <+107>:	call   0x770 <fgets@plt>
+    */
+    fgets(buffer, 128, stdin);
+    /*
     0x0000000000000a3d <+112>:	mov    DWORD PTR [rbp-0x4],0x0
+        rbp-0x4 = 4 ; int
+    */
+    int i = 0; /* rbp-0x4 */
+    /*
     0x0000000000000a44 <+119>:	jmp    0xa6a <set_username+157>
-    0x0000000000000a46 <+121>:	mov    eax,DWORD PTR [rbp-0x4]
-    0x0000000000000a49 <+124>:	cdqe   
-    0x0000000000000a4b <+126>:	movzx  ecx,BYTE PTR [rbp+rax*1-0x90]
-    0x0000000000000a53 <+134>:	mov    rdx,QWORD PTR [rbp-0x98]
-    0x0000000000000a5a <+141>:	mov    eax,DWORD PTR [rbp-0x4]
-    0x0000000000000a5d <+144>:	cdqe   
-    0x0000000000000a5f <+146>:	mov    BYTE PTR [rdx+rax*1+0x8c],cl
-    0x0000000000000a66 <+153>:	add    DWORD PTR [rbp-0x4],0x1
+    */
+    /*
     0x0000000000000a6a <+157>:	cmp    DWORD PTR [rbp-0x4],0x28
     0x0000000000000a6e <+161>:	jg     0xa81 <set_username+180>
     0x0000000000000a70 <+163>:	mov    eax,DWORD PTR [rbp-0x4]
@@ -61,37 +93,73 @@ void set_username() {
     0x0000000000000a75 <+168>:	movzx  eax,BYTE PTR [rbp+rax*1-0x90]
     0x0000000000000a7d <+176>:	test   al,al
     0x0000000000000a7f <+178>:	jne    0xa46 <set_username+121>
+    */
+    while (i <= 40 && buffer[i] != 0) {
+        /*
+        0x0000000000000a46 <+121>:	mov    eax,DWORD PTR [rbp-0x4]
+        0x0000000000000a49 <+124>:	cdqe
+        0x0000000000000a4b <+126>:	movzx  ecx,BYTE PTR [rbp+rax*1-0x90]    buffer[i]
+        0x0000000000000a53 <+134>:	mov    rdx,QWORD PTR [rbp-0x98]         message
+        0x0000000000000a5a <+141>:	mov    eax,DWORD PTR [rbp-0x4]
+        0x0000000000000a5d <+144>:	cdqe   
+        0x0000000000000a5f <+146>:	mov    BYTE PTR [rdx+rax*1+0x8c],cl     message->username[i] = buffer[i]
+        */
+        message->username[i] = buffer[i];
+        /*
+        0x0000000000000a66 <+153>:	add    DWORD PTR [rbp-0x4],0x1          
+        */
+        i++;
+    }
+    /*
     0x0000000000000a81 <+180>:	mov    rax,QWORD PTR [rbp-0x98]
-    0x0000000000000a88 <+187>:	lea    rdx,[rax+0x8c]
+    0x0000000000000a88 <+187>:	lea    rdx,[rax+0x8c]              message+140 = message->username
     0x0000000000000a8f <+194>:	lea    rax,[rip+0x165]        # 0xbfb
     0x0000000000000a96 <+201>:	mov    rsi,rdx
     0x0000000000000a99 <+204>:	mov    rdi,rax
     0x0000000000000a9c <+207>:	mov    eax,0x0
     0x0000000000000aa1 <+212>:	call   0x750 <printf@plt>
+    */
+    printf(" %s", message->username);
+    /*
     0x0000000000000aa6 <+217>:	leave  
     0x0000000000000aa7 <+218>:	ret    
     */
+    return;
 }
 
-void set_msg() {
+void set_msg(struct s_message *message) {
     /*
     0x0000000000000932 <+0>:	push   rbp
     0x0000000000000933 <+1>:	mov    rbp,rsp
     0x0000000000000936 <+4>:	sub    rsp,0x410
-    0x000000000000093d <+11>:	mov    QWORD PTR [rbp-0x408],rdi
+        total stack = 1040
+    0x000000000000093d <+11>:	mov    QWORD PTR [rbp-0x408],rdi ; arg
     0x0000000000000944 <+18>:	lea    rax,[rbp-0x400]
+        rbp-0x400 = 1024 ;  char[1024]
+    */
+    char buffer[1024]; /* rbp-0x400 */
+    /*
     0x000000000000094b <+25>:	mov    rsi,rax
     0x000000000000094e <+28>:	mov    eax,0x0
-    0x0000000000000953 <+33>:	mov    edx,0x80
+    0x0000000000000953 <+33>:	mov    edx,0x80     128
     0x0000000000000958 <+38>:	mov    rdi,rsi
     0x000000000000095b <+41>:	mov    rcx,rdx
-    0x000000000000095e <+44>:	rep stos QWORD PTR es:[rdi],rax
+    0x000000000000095e <+44>:	rep stos QWORD PTR es:[rdi],rax             ; bzero
+    */
+    bzero(buffer, 128 * 8);
+    /*
     0x0000000000000961 <+47>:	lea    rdi,[rip+0x265]        # 0xbcd
     0x0000000000000968 <+54>:	call   0x730 <puts@plt>
+    */
+    puts(">: Msg @Unix-Dude");
+    /*
     0x000000000000096d <+59>:	lea    rax,[rip+0x26b]        # 0xbdf
     0x0000000000000974 <+66>:	mov    rdi,rax
     0x0000000000000977 <+69>:	mov    eax,0x0
     0x000000000000097c <+74>:	call   0x750 <printf@plt>
+    */
+    printf("Enter your username");
+    /*
     0x0000000000000981 <+79>:	mov    rax,QWORD PTR [rip+0x201630]        # 0x201fb8
     0x0000000000000988 <+86>:	mov    rax,QWORD PTR [rax]
     0x000000000000098b <+89>:	mov    rdx,rax
@@ -99,43 +167,78 @@ void set_msg() {
     0x0000000000000995 <+99>:	mov    esi,0x400
     0x000000000000099a <+104>:	mov    rdi,rax
     0x000000000000099d <+107>:	call   0x770 <fgets@plt>
+    */
+    fgets(buffer, 1024, stdin);
+    /*
     0x00000000000009a2 <+112>:	mov    rax,QWORD PTR [rbp-0x408]
     0x00000000000009a9 <+119>:	mov    eax,DWORD PTR [rax+0xb4]
+        message + 180
     0x00000000000009af <+125>:	movsxd rdx,eax
     0x00000000000009b2 <+128>:	lea    rcx,[rbp-0x400]
-    0x00000000000009b9 <+135>:	mov    rax,QWORD PTR [rbp-0x408]
+    0x00000000000009b9 <+135>:	mov    rax,QWORD PTR [rbp-0x408]    arg
     0x00000000000009c0 <+142>:	mov    rsi,rcx
     0x00000000000009c3 <+145>:	mov    rdi,rax
     0x00000000000009c6 <+148>:	call   0x720 <strncpy@plt>
+    */
+    strncpy(message->message, buffer, message->len_message);
+    /*
     0x00000000000009cb <+153>:	leave  
     0x00000000000009cc <+154>:	ret    
     */
+    return;
 }
 
-void handle_msg() {
+void handle_msg(void) {
     /*
     0x00000000000008c0 <+0>:	push   rbp
     0x00000000000008c1 <+1>:	mov    rbp,rsp
     0x00000000000008c4 <+4>:	sub    rsp,0xc0
+     total stack = 192
     0x00000000000008cb <+11>:	lea    rax,[rbp-0xc0]
+        rbp-0xc0 = 192
+    */
+    struct s_message message; /* rbp-0xc0 */
+    /*
     0x00000000000008d2 <+18>:	add    rax,0x8c
+        192 - 140 = 52 - 12 = 40
     0x00000000000008d8 <+24>:	mov    QWORD PTR [rax],0x0
-    0x00000000000008df <+31>:	mov    QWORD PTR [rax+0x8],0x0
-    0x00000000000008e7 <+39>:	mov    QWORD PTR [rax+0x10],0x0
-    0x00000000000008ef <+47>:	mov    QWORD PTR [rax+0x18],0x0
-    0x00000000000008f7 <+55>:	mov    QWORD PTR [rax+0x20],0x0
+    0x00000000000008df <+31>:	mov    QWORD PTR [rax+0x8],0x0      8
+    0x00000000000008e7 <+39>:	mov    QWORD PTR [rax+0x10],0x0     16
+    0x00000000000008ef <+47>:	mov    QWORD PTR [rax+0x18],0x0     24
+    0x00000000000008f7 <+55>:	mov    QWORD PTR [rax+0x20],0x0     32
+        QWORD = +8 ; 32 + 8 = 40
+        equivalent to bzero 40
+        char[40]
+    */
+    bzero(message->username, 40);
+    /*
     0x00000000000008ff <+63>:	mov    DWORD PTR [rbp-0xc],0x8c
+        rbp-0xc = 12
+        int;
+    */
+    message->len_message = 140;
+    /*
     0x0000000000000906 <+70>:	lea    rax,[rbp-0xc0]
     0x000000000000090d <+77>:	mov    rdi,rax
     0x0000000000000910 <+80>:	call   0x9cd <set_username>
+    */
+    set_username(&message);
+    /*
     0x0000000000000915 <+85>:	lea    rax,[rbp-0xc0]
     0x000000000000091c <+92>:	mov    rdi,rax
     0x000000000000091f <+95>:	call   0x932 <set_msg>
+    */
+    set_msg(&message);
+    /*
     0x0000000000000924 <+100>:	lea    rdi,[rip+0x295]        # 0xbc0
     0x000000000000092b <+107>:	call   0x730 <puts@plt>
+    */
+    puts(">: Msg sent!");
+    /*
     0x0000000000000930 <+112>:	leave  
     0x0000000000000931 <+113>:	ret   
     */
+    return;
 }
 
 int main() {
@@ -144,7 +247,15 @@ int main() {
     0x0000000000000aa9 <+1>:	mov    rbp,rsp
     0x0000000000000aac <+4>:	lea    rdi,[rip+0x15d]        # 0xc10
     0x0000000000000ab3 <+11>:	call   0x730 <puts@plt>
+    */
+    puts("--------------------------------------------"\
+        "\n|   ~Welcome to l33t-m$n ~    v1337        |\n"\
+        "--------------------------------------------");
+    /*
     0x0000000000000ab8 <+16>:	call   0x8c0 <handle_msg>
+    */
+    handle_msg();
+    /*
     0x0000000000000abd <+21>:	mov    eax,0x0
     0x0000000000000ac2 <+26>:	pop    rbp
     0x0000000000000ac3 <+27>:	ret    
--- level09/walkthrough.md
+++ level09/walkthrough.md
@@ -14,3 +14,51 @@
 The program send a message?
 
 Start in GDB
+
+The program fill a structure `message` with the username and message input. Using 2 functions `set_message` and `set_username`
+
+    struct s_message {
+        char message[140];  /* message */
+        char username[40];  /* message+140 */
+        int len_message;    /* message+180 */
+    };
+
+The function `set_message()` use `strncpy` to fill the structure, with the good size, no overflow possible
+
+The function `set_username()` use a manual loop to fill the structure
+
+    int i = 0; /* rbp-0x4 */
+    while (i <= 40 && buffer[i] != 0) {
+        message->username[i] = buffer[i];
+        i++;
+    }
+
+The structure string is a buffer of `char[40]`. The loop start with `i = 0` and loop while `i <= 40`, so 41 times and not 40. This is an overflow
+
+Also, there is a non used function in the program, called `secret_backdoor`
+
+    (gdb) info functions
+    All defined functions:
+
+    Non-debugging symbols:
+    ....
+    0x000000000000088c  secret_backdoor
+    0x00000000000008c0  handle_msg
+    0x0000000000000932  set_msg
+    0x00000000000009cd  set_username
+    0x0000000000000aa8  main
+    ....
+
+This function ask an input and exec it with `system()`
+
+    void secret_backdoor() {
+        char buffer[128]; /* rbp-0x80 */
+
+        fgets(buffer, 128, stdin);
+        system(buffer);
+        return;
+    }
+
+We need to use the overflow in the `set_username` function for execute the function `secret_backdoor`
+
+
